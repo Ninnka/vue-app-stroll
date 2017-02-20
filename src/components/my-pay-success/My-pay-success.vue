0a02063dd856ddf5d40eee929e2c4a3b316b5d0f
wyao <708919996@qq.com>
2017-02-20 09:07:36 +0800

    合并分支--解决冲突

--- src/components/my-pay-success/My-pay-success.vue
+++ src/components/my-pay-success/My-pay-success.vue
@@ -1,18 +1,18 @@
 <template>
   <div id="pay">
     <div class="header">
-    	<headbar title="支付成功"  custombg="header-bg"></headbar>
+      <img src="../my/assets/back.png" alt="" class="back" @click="back">
+      <headbar title="支付成功"  custombg="header-bg"></headbar>
     </div>
     <div class="pay-content">
-    	<div>恭喜您成为VIP会员</div>
-      <button>继续购物</button><button>查看订单</button>
+      <div>恭喜您成为VIP会员</div>
+      <button>继续购物</button>
     </div>
   </div>
 </template>
 <script type="text/javascript">
 import Header from '../common/header/Header.vue';
 import router from '../../router/index.js';
-
 export default {
   data() {
     return {
@@ -20,6 +20,12 @@ export default {
   },
   components: {
     headbar: Header
+  },
+  methods: {
+    // 返回上一页
+    back() {
+      router.go(-2);
+    }
   }
 }
 </script>
@@ -28,21 +34,21 @@ export default {
   background:#D00009
 }
 #pay {
-	padding-top: .45rem;
-	font-size: .16rem;
-	height: 100%;
-	background: #f1f1f1;
+  padding-top: .45rem;
+  font-size: .16rem;
+  height: 100%;
+  background: #f1f1f1;
 }
 a{
-	color: #a3a3a3
+  color: #a3a3a3
 }
 .back {
-	position: fixed;
-	z-index: 100;
-	top: .13rem;
-	left: .1rem;
-	width: .1rem;
-	height: .18rem;
+  position: fixed;
+  z-index: 100;
+  top: .13rem;
+  left: .1rem;
+  width: .1rem;
+  height: .18rem;
 }
 .pay-content div{
   padding: 1.95rem 0 0;
@@ -54,15 +60,11 @@ a{
 .pay-content button{
   padding:.1rem .2rem;
   display: block;
-  margin: .5rem auto 0;
+  margin: .5rem auto;
   border:none;
   outline: none;
   background: #E42837;
   color: #fff;font-size: .14rem;
   border-radius: 5px;
-}
-.pay-content button:last-child{
-  margin: .3rem auto 0;
-  background: #FF5636;
 }
 </style>
